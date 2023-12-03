55a92bf33a410dd3f1f1a624f423954c13f27f7e
DivyaMurugesan3265 <d.murugesan@lakeba.com>
2023-12-03 22:45:45 +0530

    my portfolio

---
+++ src/components/About/Techstack.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { CgCPlusPlus } from "react-icons/cg";
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejs,
+  DiMongodb,
+  DiPython,
+  DiGit,
+  DiJava,
+} from "react-icons/di";
+import {
+  SiRedis,
+  SiFirebase,
+  SiNextdotjs,
+  SiSolidity,
+  SiPostgresql,
+} from "react-icons/si";
+import { FaHtml5, FaCss3 } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io";
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <IoLogoJavascript />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <FaCss3 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaHtml5 />
+      </Col>
+    </Row>
+  );
+}
+
+export default Techstack;
